5c5b2c69b4fcf59c6bdeca88c470c4feb0b53192
Alexey <a.koles1994@mail.ru>
2015-01-12 17:52:31 +0500

    Plugin.Original
    Original

---
+++ js/koko.js
@@ -0,0 +1,18 @@
+(function($){	
+$( document ).ready( function( )
+{
+	$( "body" ).FancyIndex(/* {
+				hideWhenInactive: true,
+				focusInitally: true,
+				focusOnResize: true,
+				focusOnScroll: true,
+				focusOnHover: true,
+				focusTimeout: 1000,
+				firstOnly: false,
+				forceLastActive: true,
+				scrollToDuration: 1000,
+				scrollOnClick: true,
+				maxPrioritizedItems: 3
+	}*/);
+} );
+})(jQuery);
